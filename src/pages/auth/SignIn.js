830db914eaa74b417e8614bc9c6ae681fe84a399
sumgu <sum.dv@geekup.vn>
2022-08-27 14:37:07 +0700

    Add loading oauth button

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -5,6 +5,7 @@ import FacebookLogin from 'react-facebook-login';
 import { toast } from 'react-toastify';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import LoadingIcons from 'react-loading-icons'
 import styles from './SignIn.module.scss';
 import ImageCarousel from "components/ImageCarousel";
 
@@ -16,6 +17,8 @@ export default function SignIn() {
     const navigate = useNavigate();
     const [showScrollImg, setShowScrollImg] = useState(true);
     const [userInfo, setUserInfo] = useUserInfo();
+    const [isOauthFacebookLoading, setIsOauthFacebookLoading] = useState(false);
+    const [isOauthGoogleLoading, setIsOauthGoogleLoading] = useState(false);
     const [message] = useState('');
 
     useEffect(() => {
@@ -38,11 +41,10 @@ export default function SignIn() {
                 callback: oauthGoogleCallback,
             })
             window.google.accounts.id.prompt();
-
             window.google.accounts.id.renderButton(
                 document.getElementById("login-google-button"), {
-                    type: "icon"
-                }
+                type: "icon"
+            }
             )
         }
     }, [])
@@ -53,8 +55,9 @@ export default function SignIn() {
                 toast.error("Xảy ra lỗi, vui lòng thử lại!");
                 return;
             }
-
+            setIsOauthFacebookLoading(true);
             const userInfo = await request.authenticate(authRouteList.facebook, { accessToken: res.accessToken });
+            setIsOauthFacebookLoading(false);
             if (userInfo) {
                 setUserInfo(userInfo);
                 navigate('/');
@@ -70,8 +73,9 @@ export default function SignIn() {
                 toast.error("Xảy ra lỗi, vui lòng thử lại!");
                 return;
             }
-
+            setIsOauthGoogleLoading(true);
             const userInfo = await request.authenticate(authRouteList.google, { idToken: res.credential });
+            setIsOauthGoogleLoading(false);
             if (userInfo) {
                 setUserInfo(userInfo);
                 navigate('/');
@@ -123,20 +127,37 @@ export default function SignIn() {
                         cssClass={clsx(styles.loginFacebookButton, styles.oauthButton)}
                         icon={
                             <div className={clsx(styles.svg, styles.fbLogin)}>
-                                <svg xmlns="http://www.w3.org/20svg" viewBox="0 0 216 216" className="_5h0m" color="#FFFFFF">
-                                    <path fill="#FFFFFF" d="M204.1 0H11.9C5.3 0 0 5.3 0 11.9v192.2c0 6.6 5.3 11.9 11.9
+                                {
+                                    isOauthFacebookLoading ? <LoadingIcons.TailSpin style={{ top: 1 }} /> : (
+                                        <svg xmlns="http://www.w3.org/20svg" viewBox="0 0 216 216" className="_5h0m" color="#FFFFFF">
+                                            <path fill="#FFFFFF" d="M204.1 0H11.9C5.3 0 0 5.3 0 11.9v192.2c0 6.6 5.3 11.9 11.9
                                             11.9h103.5v-83.6H87.2V99.8h28.1v-24c0-27.9 17-43.1 41.9-43.1
                                             11.9 0 22.2.9 25.2 1.3v29.2h-17.3c-13.5 0-16.2 6.4-16.2
                                             15.9v20.8h32.3l-4.2 32.6h-28V216h55c6.6 0 11.9-5.3
                                             11.9-11.9V11.9C216 5.3 210.7 0 204.1 0z">
-                                    </path>
-                                </svg>
+                                            </path>
+                                        </svg>
+                                    )
+                                }
                             </div>
                         }
                         textButton=""
+                        isDisabled={isOauthFacebookLoading}
                     />
 
-                    <div id="login-google-button" className={clsx(styles.svg, styles.googleLogin, styles.oauthButton)}></div>
+                    <div
+                        id="login-google-button"
+                        className={clsx(styles.svg, styles.googleLogin, styles.oauthButton)}
+                        style={{ display: isOauthGoogleLoading ? "none" : "inline-block" }}
+                    >
+                    </div>
+                    {
+                        isOauthGoogleLoading ? (
+                            <div className={clsx(styles.svg, styles.googleLogin, styles.oauthButton)} style={{ border: "solid 0.4px #9d9d9d" }}>
+                                <LoadingIcons.TailSpin stroke="red" style={{ top: 1 }} />
+                            </div>
+                        ) : null
+                    }
                 </div>
                 <img
                     className={clsx("arrowDownIcon", {
