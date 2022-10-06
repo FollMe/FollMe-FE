0d156bdf5428ccd5d413fe3ebf6263dba9ffd872
sumgu <sum.dv@geekup.vn>
2022-10-06 10:39:01 +0700

    Add button add new blog

--- src/pages/blog/BlogList.js
+++ src/pages/blog/BlogList.js
@@ -1,16 +1,31 @@
+import { useEffect } from 'react';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
 import Paper from '@mui/material/Paper';
 import styles from "./BlogList.module.scss";
 
 export default function BlogList() {
+    useEffect(() => {
+        document.title = "Blog | FollMe";
+    }, [])
+
     return (
         <div className="containerMain">
             <div className="containerStory">
-                <Typography className={styles.txtPageTitle} gutterBottom variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
-                    Blogs
-                </Typography>
-
-                <Paper variant="outlined" sx={{ borderRadius: '8px', padding: '16px' }}>
+                <Stack direction="row" justifyContent="space-between" alignItems="center">
+                    <Typography className={styles.txtPageTitle} gutterBottom variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
+                        Blogs
+                    </Typography>
+                    <Tooltip placement='left' title={<Typography fontSize={"1.3rem"}>Write new blog</Typography>}>
+                        <IconButton className={styles.btnAddNewBlog} variant="outlined">
+                            <AddIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Stack>
+                <Paper variant="outlined" sx={{ marginTop: '20px', borderRadius: '8px', padding: '16px' }}>
                     There is no blog
                 </ Paper>
             </div>
